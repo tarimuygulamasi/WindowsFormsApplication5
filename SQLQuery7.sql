0f6f96351fa6a988d00d819f78633a346e9b5396
XPRE1 <56554688+XPRE1@users.noreply.github.com>
2019-11-21 01:28:15 +0300

    Delete SQLQuery7.sql

--- SQLQuery7.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM identityornek;
-INSERT INTO identityornek(column2,column3) VALUES ('Cheese','Pizza');
-INSERT INTO identityornek(column2,column3) VALUES ('Ham','Pizza');
-INSERT INTO identityornek(column2,column3) VALUES ('Pepperoni','Pizza');
-INSERT INTO identityornek(column2,column3) VALUES ('Sausage','Pizza');
-INSERT INTO identityornek(column2,column3) VALUES ('Mushroom','Pizza');
